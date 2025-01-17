cb99ef5bf0daf808d487abf030bc70aa9d765686
tosiab <mtabednarz@gmail.com>
2025-01-17 14:54:16 -0500

    jdjfehsdk

---
+++ Lesson2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Lesson2{
+    public static void main(String[]args){
+        ArrayList<Integer> multiples = new ArrayList<Integer>();
+        int m = 0;
+        for(int i = 0; i<101; i++){
+            multiples.add(m);
+            m+=3;
+        }
+        System.out.println(multiples);
+
+        for(int i = 0; i<multiples.size(); i++){
+            if((multiples.get(i)%2)==0){
+                multiples.remove(i);
+            }
+        }
+        System.out.println(multiples);
+    }
+
+}
